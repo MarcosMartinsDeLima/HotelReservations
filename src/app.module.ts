c03e8e9cd53df402c3adc6d2a515b1ded7f31429
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-08-17 23:40:22 -0300

    feat:estrutura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FuncionarioModule } from './admin/funcionario.module';
+import { Funcionario } from './admin/entities/funcionarioentity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({ 
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:"root",
+      password:"secreto123",
+      database:"hotel" ,
+      entities:[Funcionario],
+      synchronize:true
+    }),
+    FuncionarioModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
